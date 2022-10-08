674eb2910d2bc09627f4fcd6b5e6c6257666f4f1
Zachary Sain <zachsain@gmail.com>
2022-10-08 12:59:44 -0500

    commit tree f2cdf07236d2a9d37b887fb33737f64625f65c28

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  resources :users
+
+  post "/signup", to: "users#create"
+  post "/login", to: "sessions#create"
+
+  delete "/logout", to: "sessions#destroy"
+
+  get "/me", to: "users#show"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
